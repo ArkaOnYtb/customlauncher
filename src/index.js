769a18cc0257ecc4ea2becd56a33fc361ccf9fa1
KangPvP <82943439+KangPvP@users.noreply.github.com>
2023-08-11 21:02:56 +0200

    LoadBar

--- src/index.js
+++ src/index.js
@@ -66,9 +66,9 @@ setupTitlebar();
 	mainWindow.webContents.setDevToolsWebContents(devtools.webContents)
 	mainWindow.webContents.openDevTools({ mode: 'detach' })
 
-	Menu.setApplicationMenu(null)
+	Menu.setApplicationMenu(exampleMenuTemplate)
 
-	//attachTitlebarToWindow(mainWindow);
+	attachTitlebarToWindow(mainWindow);
 
 };
 
@@ -136,4 +136,122 @@ ipcMain.on('launchGame', async (event, args) => {
 module.exports = {
   	emit: emit
 }
+
+const exampleMenuTemplate = [
+	{
+		label: 'Simple O&ptions',
+		submenu: [
+			{
+				label: 'Quit',
+				click: () => app.quit()
+			},
+			{
+				label: 'Radio1',
+				type: 'radio',
+				checked: true
+			},
+		]
+	},
+	{
+		label: 'A&dvanced Options',
+		submenu: [
+			{
+				label: 'Quit',
+				click: () => app.quit()
+			},
+			{
+				label: 'Radio1',
+				type: 'radio',
+				checked: true
+			},
+			{
+				label: 'Radio2',
+				type: 'radio'
+			},
+			{
+				label: 'Checkbox1',
+				type: 'checkbox',
+				checked: true,
+				click: (item) => {
+					console.log('item is checked? ' + item.checked)
+				}
+			},
+			{ type: 'separator' },
+			{
+				label: 'Checkbox2',
+				type: 'checkbox',
+				checked: false,
+				click: (item) => {
+					console.log('item is checked? ' + item.checked)
+				}
+			},
+			{
+				label: 'Radio Test',
+				submenu: [
+					{
+						label: 'S&ample Checkbox',
+						type: 'checkbox',
+						checked: true
+					},
+					{
+						label: 'Radio1',
+						checked: true,
+						type: 'radio'
+					},
+					{
+						label: 'Radio2',
+						type: 'radio'
+					},
+					{
+						label: 'Radio3',
+						type: 'radio'
+					},
+					{ type: 'separator' },
+					{
+						label: 'Radio1',
+						checked: true,
+						type: 'radio'
+					},
+					{
+						label: 'Radio2',
+						type: 'radio'
+					},
+					{
+						label: 'Radio3',
+						type: 'radio'
+					}
+				]
+			},
+			{
+				label: 'zoomIn',
+				role: 'zoomIn'
+			},
+			{
+				label: 'zoomOut',
+				role: 'zoomOut'
+			},
+			{
+				label: 'Radio1',
+				type: 'radio'
+			},
+			{
+				label: 'Radio2',
+				checked: true,
+				type: 'radio'
+			}
+		]
+	},
+	{
+		label: '&View',
+		submenu: [
+			{ role: 'reload' },
+			{ role: 'forceReload' },
+			{ type: 'separator' },
+			{ role: 'zoomIn' },
+			{ role: 'zoomOut' },
+			{ role: 'resetZoom' },
+			{ role: 'toggleDevTools' }
+		]
+	}
+]
 
